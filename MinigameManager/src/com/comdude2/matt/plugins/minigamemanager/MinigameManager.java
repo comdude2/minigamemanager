1271e11e09de14131cd551e57288ad9c6309bd6f
comdude2 <matt@comdude2.com>
2014-05-08 00:50:52 +0100

    CommandManager

--- MinigameManager/src/com/comdude2/matt/plugins/minigamemanager/MinigameManager.java
+++ MinigameManager/src/com/comdude2/matt/plugins/minigamemanager/MinigameManager.java
@@ -3,7 +3,7 @@ package com.comdude2.matt.plugins.minigamemanager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class MinigameManager extends JavaPlugin{
-
+	
 	public void onEnable(){
 		
 	}
