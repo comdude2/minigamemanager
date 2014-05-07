db6e0ba422f01c0ccba31de88f8ee0950d6200a2
comdude2 <matt@comdude2.com>
2014-05-08 00:58:34 +0100

    Stuff

---
+++ MinigameManager/src/com/comdude2/matt/plugins/minigamemanager/MinigameController.java
@@ -0,0 +1,13 @@
+package com.comdude2.matt.plugins.minigamemanager;
+
+import org.bukkit.plugin.Plugin;
+
+public class MinigameController {
+
+	protected Plugin plugin;
+	
+	public MinigameController(Plugin mplugin){
+		plugin = mplugin;
+	}
+	
+}
