a03ee7f6f72915346b8f421bd75d4c3bb55b463b
comdude2 <matt@comdude2.com>
2014-05-08 00:34:56 +0100

    Back to basics

--- MinigameManager/src/com/comdude2/matt/plugins/minigamemanager/MinigameManager.java
+++ MinigameManager/src/com/comdude2/matt/plugins/minigamemanager/MinigameManager.java
@@ -5,11 +5,11 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class MinigameManager extends JavaPlugin{
 
 	public void onEnable(){
-		int x;
+		
 	}
 	
 	public void onDisable(){
-		String chocolate;
+		
 	}
 	
 	
