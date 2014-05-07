7f2d97e67fb57e505a00088babc34d7153e81d44
aidenmagrath@gmail.com <Ethoslab313>
2014-05-08 08:32:52 +0930

    Aiden Wrote This

--- MinigameManager/src/com/comdude2/matt/plugins/minigamemanager/MinigameManager.java
+++ MinigameManager/src/com/comdude2/matt/plugins/minigamemanager/MinigameManager.java
@@ -5,7 +5,9 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class MinigameManager extends JavaPlugin{
 
 	public void onEnable(){
-		
+		String x = "Hello, World!";
+		String y = "This is a test";
+		String z = "Goodbye, World!";
 	}
 	
 	public void onDisable(){
