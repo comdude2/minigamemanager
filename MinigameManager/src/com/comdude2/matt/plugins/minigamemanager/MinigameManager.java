d9c5e4cb229271510198be3b2e932f85224cbe92
comdude2 <matt@comdude2.com>
2014-05-07 20:16:06 +0100

    Start of project

---
+++ MinigameManager/src/com/comdude2/matt/plugins/minigamemanager/MinigameManager.java
@@ -0,0 +1,17 @@
+package com.comdude2.matt.plugins.minigamemanager;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MinigameManager extends JavaPlugin{
+
+	public void onEnable(){
+		
+	}
+	
+	public void onDisable(){
+		
+	}
+	
+	
+	
+}
