ec9806e6b3ebd87c769b7a46d61c5ade0daa28f9
comdude2 <matt@comdude2.com>
2014-05-08 23:36:16 +0100

    Meh

--- MinigameManager/src/com/comdude2/minigame/managers/ArenaManager.java
+++ MinigameManager/src/com/comdude2/minigame/managers/ArenaManager.java
@@ -1,5 +1,18 @@
 package com.comdude2.minigame.managers;
 
+import java.util.LinkedList;
+import java.util.List;
+
+import com.comdude2.minigame.main.Arena;
+
 public class ArenaManager {
 
+	private List <Arena> arenas = new LinkedList <Arena> ();
+	
+	public ArenaManager(){
+		
+	}
+	
+	public void 
+	
 }
