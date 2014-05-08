78e46988bc56736d2878465d574418e9a002f22e
comdude2 <matt@comdude2.com>
2014-05-08 16:51:34 +0100

    Added simple command structure

--- MinigameManager/src/com/comdude2/matt/plugins/minigamemanager/CommandManager.java
+++ MinigameManager/src/com/comdude2/matt/plugins/minigamemanager/CommandManager.java
@@ -1,5 +1,15 @@
 package com.comdude2.matt.plugins.minigamemanager;
 
-public class CommandManager {
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class CommandManager implements CommandExecutor{
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		
+		return false;
+	}
 
 }
