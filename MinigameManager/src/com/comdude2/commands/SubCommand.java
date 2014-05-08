e7bc277efe3c8c89350f84a045ece7aa17617642
aidenmagrath@gmail.com <Ethoslab313>
2014-05-09 07:03:54 +0930

    Test

---
+++ MinigameManager/src/com/comdude2/commands/SubCommand.java
@@ -0,0 +1,13 @@
+package com.comdude2.commands;
+
+import org.bukkit.entity.Player;
+
+public abstract class SubCommand {
+	public abstract void onCommand(Player p, String[] args);
+	
+	public abstract String name();
+	
+	public abstract String info();
+	
+	public abstract String[] aliases();
+}
