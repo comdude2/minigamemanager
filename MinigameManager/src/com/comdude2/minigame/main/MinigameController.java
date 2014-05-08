73090c97fc52594099ec2cbf93fee2f57a4af7f1
aidenmagrath@gmail.com <Ethoslab313>
2014-05-09 07:44:44 +0930

    .

--- MinigameManager/src/com/comdude2/minigame/main/MinigameController.java
+++ MinigameManager/src/com/comdude2/minigame/main/MinigameController.java
@@ -2,25 +2,23 @@ package com.comdude2.minigame.main;
 
 import java.util.LinkedList;
 import java.util.List;
-import org.bukkit.plugin.java.JavaPlugin;
+
+import org.bukkit.plugin.Plugin;
+
 import com.comdude2.minigame.managers.CommandManager;
 import com.comdude2.minigame.managers.MessageManager;
 
 public class MinigameController {
 
-	protected JavaPlugin plugin;
-	protected CommandManager commandmanager;
+	protected Plugin plugin;
 	protected MessageManager messagemanager;
+	
 	@SuppressWarnings("unused")
 	private List <Minigame> minigames = new LinkedList <Minigame> ();
 	
-	public MinigameController(JavaPlugin mplugin){
+	public MinigameController(Plugin mplugin){
 		plugin = mplugin;
-		commandmanager = new CommandManager();
 		messagemanager = new MessageManager();
-		
-		commandmanager.setup();
-		plugin.getCommand("mm").setExecutor(commandmanager);
 	}
 	
 	public void registerMinigame(Minigame minigame){
--- MinigameManager/src/com/comdude2/minigame/managers/MinigameManager.java
+++ MinigameManager/src/com/comdude2/minigame/managers/MinigameManager.java
@@ -6,12 +6,19 @@ import com.comdude2.minigame.main.MinigameController;
 
 public class MinigameManager extends JavaPlugin{
 	
+	protected CommandManager commandmanager;
+	
 	//Private and not protected to prevent unauthorised usage (stops outside plugins interfacing)
 	@SuppressWarnings("unused")
 	private MinigameController minicontroller;
 	
 	public void onEnable(){
 		minicontroller = new MinigameController(this);
+		commandmanager = new CommandManager();
+		
+		commandmanager.setup();
+		getCommand("mm").setExecutor(commandmanager);
+		
 	}
 	
 	public void onDisable(){
